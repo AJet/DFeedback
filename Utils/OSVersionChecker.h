c7e93d1f26cbf24b7ba2c66db258807d9c5471c1
Oleg Krupnov <oleg.krupnov@gmail.com>
2012-09-07 19:23:00 +0300

    Minor update for Mountain Lion

--- Utils/OSVersionChecker.h
+++ Utils/OSVersionChecker.h
@@ -13,7 +13,8 @@ typedef enum OSVersion : NSUInteger
     OSVersion_Unknown = 0,
     OSVersion_Leopard = 0x1050,
     OSVersion_SnowLeopard = 0x1060,
-    OSVersion_Lion = 0x1070
+    OSVersion_Lion = 0x1070,
+    OSVersion_MountainLion = 0x1080
 } OSVersion;
 
 //-------------------------------------------------------------------------------------------------

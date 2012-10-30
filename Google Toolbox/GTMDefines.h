27e438e62166da97b119adcb06596ff0577ba4e3
Oleg Krupnov <oleg.krupnov@gmail.com>
2012-10-30 11:22:10 +0200

    Replaced some deprecated methods (in Google code)

--- Google Toolbox/GTMDefines.h
+++ Google Toolbox/GTMDefines.h
@@ -96,8 +96,8 @@ extern void _GTMUnitTestDevLog(NSString *format, ...);
 do {                                                                   \
 if (!(condition)) {                                                  \
 [[NSAssertionHandler currentHandler]                               \
-handleFailureInFunction:[NSString stringWithCString:__PRETTY_FUNCTION__] \
-file:[NSString stringWithCString:__FILE__]  \
+handleFailureInFunction:[NSString stringWithUTF8String:__PRETTY_FUNCTION__] \
+file:[NSString stringWithUTF8String:__FILE__]  \
 lineNumber:__LINE__                               \
 description:__VA_ARGS__];                          \
 }                                                                    \

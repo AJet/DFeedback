ca90f1860114f137fd3faeb2a72d33f0522e2a4d
Oleg Krupnov <oleg.krupnov@gmail.com>
2014-08-18 19:04:31 +0300

    Updated OSX version checker to work on Yosemite

--- Helpers/OSXVersion.m
+++ Helpers/OSXVersion.m
@@ -18,30 +18,41 @@ static SoftwareVersion* _version = nil;
 {
 	if (_generation == OSXGeneration_Unknown)
 	{
-		SInt32 version = 0;
-		if (Gestalt(gestaltSystemVersion, &version) == noErr)
-		{
-			if (version >= 0x1090)
-			{
-				_generation = OSXGeneration_Mavericks;
-			}
-			else if (version >= 0x1080)
-			{
-				_generation = OSXGeneration_MountainLion;
-			}
-			else if (version >= 0x1070)
-			{
-				_generation = OSXGeneration_Lion;
-			}
-			else if (version >= 0x1060)
-			{
-				_generation = OSXGeneration_SnowLeopard;
-			}
-			else if (version >= 0x1050)
-			{
-				_generation = OSXGeneration_Leopard;
-			}
-		}	
+        SInt32 major = 0;
+        if (Gestalt(gestaltSystemVersionMajor, &major) == noErr)
+        {
+            if (major == 10)
+            {
+                SInt32 minor = 0;
+                if (Gestalt(gestaltSystemVersionMinor, &minor) == noErr)
+                {
+                    switch (minor)
+                    {
+                        case 10:
+                            _generation = OSXGeneration_Yosemite;
+                            break;
+                        case 9:
+                            _generation = OSXGeneration_Mavericks;
+                            break;
+                        case 8:
+                            _generation = OSXGeneration_MountainLion;
+                            break;
+                        case 7:
+                            _generation = OSXGeneration_Lion;
+                            break;
+                        case 6:
+                            _generation = OSXGeneration_SnowLeopard;
+                            break;
+                        case 5:
+                            _generation = OSXGeneration_Leopard;
+                            break;
+                            
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
 	}
 	return _generation;
 }
@@ -52,15 +63,20 @@ static SoftwareVersion* _version = nil;
     if (_version == nil)
     {
         SInt32 majorVersion = 0;
-        Gestalt(gestaltSystemVersionMajor, &majorVersion);
-        SInt32 minorVersion = 0;
-        Gestalt(gestaltSystemVersionMinor, &minorVersion);
-        SInt32 buildVersion = 0;
-        Gestalt(gestaltSystemVersionBugFix, &buildVersion);
-
-        NSUInteger numbers[3] = {majorVersion, minorVersion, buildVersion};
-        _version = [[SoftwareVersion versionFromNumbers:numbers count:3] retain];
-        [_version makeDisplayName];
+        if (Gestalt(gestaltSystemVersionMajor, &majorVersion) == noErr)
+        {
+            SInt32 minorVersion = 0;
+            if (Gestalt(gestaltSystemVersionMinor, &minorVersion) == noErr)
+            {
+                SInt32 buildVersion = 0;
+                if (Gestalt(gestaltSystemVersionBugFix, &buildVersion) == noErr)
+                {
+                    NSUInteger numbers[3] = {majorVersion, minorVersion, buildVersion};
+                    _version = [[SoftwareVersion versionFromNumbers:numbers count:3] retain];
+                    [_version makeDisplayName];
+                }
+            }
+        }
     }
     return _version;
 }

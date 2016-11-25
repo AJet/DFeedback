62737f09604d88decbc13156647b21d5d8a5b992
Oleg Krupnov <oleg.krupnov@gmail.com>
2016-11-25 15:29:31 +0200

    Replaced deprecated code

--- Helpers/OSXVersion.m
+++ Helpers/OSXVersion.m
@@ -21,55 +21,69 @@ static SoftwareVersion* _version = nil;
         OSXGeneration result = _generation;
 
         SInt32 majorVersion = 0;
+        SInt32 minorVersion = 0;
+        BOOL hasVersion = NO;
+        
+        // pre-yosemite deprecated way
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_10
         if (Gestalt(gestaltSystemVersionMajor, &majorVersion) == noErr)
         {
-            SInt32 minorVersion = 0;
             if (Gestalt(gestaltSystemVersionMinor, &minorVersion) == noErr)
             {
-                if (majorVersion == 10)
+                hasVersion = YES;
+            }
+        }
+#else
+        NSOperatingSystemVersion version = NSProcessInfo.processInfo.operatingSystemVersion;
+        majorVersion = (SInt32)version.majorVersion;
+        minorVersion = (SInt32)version.minorVersion;
+        hasVersion = YES;
+        
+#endif
+        if (hasVersion)
+        {
+            if (majorVersion == 10)
+            {
+                switch (minorVersion)
                 {
-                    switch (minorVersion)
-                    {
-                        case 12:
-                            result = OSXGeneration_Sierra;
-                            break;
-                        case 11:
-                            result = OSXGeneration_ElCapitan;
-                            break;
-                        case 10:
-                            result = OSXGeneration_Yosemite;
-                            break;
-                        case 9:
-                            result = OSXGeneration_Mavericks;
-                            break;
-                        case 8:
-                            result = OSXGeneration_MountainLion;
-                            break;
-                        case 7:
-                            result = OSXGeneration_Lion;
-                            break;
-                        case 6:
-                            result = OSXGeneration_SnowLeopard;
-                            break;
-                        case 5:
-                            result = OSXGeneration_Leopard;
-                            break;
-                            
-                        default:
-                            break;
-                    }
-                }
-                
-                if (result == OSXGeneration_Unknown)
-                {
-                    // an attempt to provide future compatibility
-                    if (majorVersion >= 0 && majorVersion <= 0xFF && minorVersion >= 0 && minorVersion <= 0xFF)
-                    {
-                        result = (OSXGeneration)((majorVersion << 8) | minorVersion);
-                    }
+                    case 12:
+                        result = OSXGeneration_Sierra;
+                        break;
+                    case 11:
+                        result = OSXGeneration_ElCapitan;
+                        break;
+                    case 10:
+                        result = OSXGeneration_Yosemite;
+                        break;
+                    case 9:
+                        result = OSXGeneration_Mavericks;
+                        break;
+                    case 8:
+                        result = OSXGeneration_MountainLion;
+                        break;
+                    case 7:
+                        result = OSXGeneration_Lion;
+                        break;
+                    case 6:
+                        result = OSXGeneration_SnowLeopard;
+                        break;
+                    case 5:
+                        result = OSXGeneration_Leopard;
+                        break;
+                        
+                    default:
+                        break;
                 }
             }
-
+            
+            if (result == OSXGeneration_Unknown)
+            {
+                // an attempt to provide future compatibility
+                if (majorVersion >= 0 && majorVersion <= 0xFF && minorVersion >= 0 && minorVersion <= 0xFF)
+                {
+                    result = (OSXGeneration)((majorVersion << 8) | minorVersion);
+                }
+            }
         }
         
         _generation = result;
@@ -83,21 +97,38 @@ static SoftwareVersion* _version = nil;
     if (_version == nil)
     {
         SInt32 majorVersion = 0;
+        SInt32 minorVersion = 0;
+        SInt32 buildVersion = 0;
+        BOOL hasVersion = NO;
+
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_10
         if (Gestalt(gestaltSystemVersionMajor, &majorVersion) == noErr)
         {
-            SInt32 minorVersion = 0;
             if (Gestalt(gestaltSystemVersionMinor, &minorVersion) == noErr)
             {
-                SInt32 buildVersion = 0;
                 if (Gestalt(gestaltSystemVersionBugFix, &buildVersion) == noErr)
                 {
-                    NSUInteger numbers[3] = {(NSUInteger)majorVersion, (NSUInteger)minorVersion, (NSUInteger)buildVersion};
-                    _version = [[SoftwareVersion makeVersionFromNumbers:numbers count:3] retain];
-                    [_version makeDisplayName];
+                    hasVersion = YES;
                 }
             }
         }
+#else
+        NSOperatingSystemVersion version = NSProcessInfo.processInfo.operatingSystemVersion;
+        majorVersion = (SInt32)version.majorVersion;
+        minorVersion = (SInt32)version.minorVersion;
+        buildVersion = (SInt32)version.patchVersion;
+        hasVersion = YES;
+
+#endif
+        
+        if (hasVersion)
+        {
+            NSUInteger numbers[3] = {(NSUInteger)majorVersion, (NSUInteger)minorVersion, (NSUInteger)buildVersion};
+            _version = [[SoftwareVersion makeVersionFromNumbers:numbers count:3] retain];
+            [_version makeDisplayName];
+        }
     }
+    
     return _version;
 }
 

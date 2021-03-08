fb30ab1a17b1ef026ca2296ea4d1eb83a2c1c372
Oleg Krupnov <oleg.krupnov@gmail.com>
2021-03-08 16:14:35 +0200

    Updated links in comments

--- Helpers/EmailValidation.m
+++ Helpers/EmailValidation.m
@@ -1,6 +1,6 @@
 //-------------------------------------------------------------------------------------------------
-// Copyright (c) 2008 DaisyDisk Team: <http://www.daisydiskapp.com>
-// Some rights reserved: <http://opensource.org/licenses/mit-license.php>
+// Copyright (c) 2008 DaisyDisk Team: <https://daisydiskapp.com>
+// Some rights reserved: <https://opensource.org/licenses/mit-license.php>
 //-------------------------------------------------------------------------------------------------
 
 #import "EmailValidation.h"
